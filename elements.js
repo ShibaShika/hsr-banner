07ead6aa6fa124e242ce26661205daa4b880ff15
ShibaShika <40266069+ShibaShika@users.noreply.github.com>
2026-07-21 01:25:01 +0800

    Create elements.js

---
+++ elements.js
@@ -0,0 +1,10 @@
+const ELEMENT_CONFIG = {
+    "火": { class: "bg-火" },
+    "冰": { class: "bg-冰" },
+    "雷": { class: "bg-雷" },
+    "風": { class: "bg-風" },
+    "物理": { class: "bg-物理" },
+    "量子": { class: "bg-量子" },
+    "虛數": { class: "bg-虛數" },
+    "未知": { class: "bg-未知" }
+};
