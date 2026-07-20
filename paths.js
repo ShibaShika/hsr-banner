740c025bf14887a654b2664664b118b67a43c177
ShibaShika <40266069+ShibaShika@users.noreply.github.com>
2026-07-21 01:24:23 +0800

    Create paths.js

---
+++ paths.js
@@ -0,0 +1,11 @@
+const PATH_ICONS = {
+    "毀滅": "https://raw.githubusercontent.com/Mar-7th/StarRailRes/master/icon/path/Destruction.png",
+    "巡獵": "https://raw.githubusercontent.com/Mar-7th/StarRailRes/master/icon/path/Hunt.png",
+    "智識": "https://raw.githubusercontent.com/Mar-7th/StarRailRes/master/icon/path/Erudition.png",
+    "同諧": "https://raw.githubusercontent.com/Mar-7th/StarRailRes/master/icon/path/Harmony.png",
+    "虛無": "https://raw.githubusercontent.com/Mar-7th/StarRailRes/master/icon/path/Nihility.png",
+    "存護": "https://raw.githubusercontent.com/Mar-7th/StarRailRes/master/icon/path/Preservation.png",
+    "豐饒": "https://raw.githubusercontent.com/Mar-7th/StarRailRes/master/icon/path/Abundance.png",
+    "記憶": "https://raw.githubusercontent.com/Mar-7th/StarRailRes/master/icon/path/Remembrance.png",
+    "歡愉": "https://raw.githubusercontent.com/Mar-7th/StarRailRes/master/icon/path/Elation.png"
+};
