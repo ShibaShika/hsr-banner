c8bfcb9da9bf8cad38f6077cefca8c9d8c1ea3fd
ShibaShika <40266069+ShibaShika@users.noreply.github.com>
2026-07-21 01:34:42 +0800

    Update elements.js

--- elements.js
+++ elements.js
@@ -1,10 +1,12 @@
 const ELEMENT_CONFIG = {
+    "物理": { class: "bg-物理" },
     "火": { class: "bg-火" },
     "冰": { class: "bg-冰" },
     "雷": { class: "bg-雷" },
     "風": { class: "bg-風" },
-    "物理": { class: "bg-物理" },
     "量子": { class: "bg-量子" },
     "虛數": { class: "bg-虛數" },
     "未知": { class: "bg-未知" }
 };
+
+const ELEM_ORDER = ["物理", "火", "冰", "雷", "風", "量子", "虛數", "未知"];
