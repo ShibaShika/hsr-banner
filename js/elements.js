219646edb98b07adaabf084edf1d68e99373c4ea
ShibaShika <40266069+ShibaShika@users.noreply.github.com>
2026-07-27 14:19:03 +0800

    Update elements.js

--- js/elements.js
+++ js/elements.js
@@ -9,4 +9,15 @@ const ELEMENT_CONFIG = {
     "未知": { class: "bg-未知" }
 };
 
+// ===== 補上屬性圖示資料庫 (StarRailRes 官方圖庫網址) =====
+const ELEM_ICONS = {
+    "物理": "https://raw.githubusercontent.com/Mar-7th/StarRailRes/master/icon/element/Physical.png",
+    "火": "https://raw.githubusercontent.com/Mar-7th/StarRailRes/master/icon/element/Fire.png",
+    "冰": "https://raw.githubusercontent.com/Mar-7th/StarRailRes/master/icon/element/Ice.png",
+    "雷": "https://raw.githubusercontent.com/Mar-7th/StarRailRes/master/icon/element/Thunder.png",
+    "風": "https://raw.githubusercontent.com/Mar-7th/StarRailRes/master/icon/element/Wind.png",
+    "量子": "https://raw.githubusercontent.com/Mar-7th/StarRailRes/master/icon/element/Quantum.png",
+    "虛數": "https://raw.githubusercontent.com/Mar-7th/StarRailRes/master/icon/element/Imaginary.png"
+};
+
 const ELEM_ORDER = ["物理", "火", "冰", "雷", "風", "量子", "虛數", "未知"];
